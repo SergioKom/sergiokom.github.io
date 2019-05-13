3f90ae0ede629bae0caf997d104115c2a57a3613
SergioKom <forcette@gmail.com>
2019-05-13 11:29:28 +0300

    test_9

--- js.js
+++ js.js
@@ -4,12 +4,22 @@ $(function(){
         event.preventDefault ();
         if ($(this).hasClass('opened')) {
             $(this).removeClass('opened');
-            $('.code_link_box').hide();
+            $(this).children('.code_link_box').hide();
         }
         else {
             $(this).addClass('opened');
-            $('.code_link_box').show();
+            $(this).children('.code_link_box').show();
         }
     });
+
+    // $('.icon_link').on('mouseover', function () {
+    //     event.preventDefault ();
+    //     $(this).children('.code_link_box').show();
+    // });
+    // $('.icon_link').on('mouseout', function () {
+    //     event.preventDefault ();
+    //     $(this).children('.code_link_box').hide();  
+    // });
+
 
 })
